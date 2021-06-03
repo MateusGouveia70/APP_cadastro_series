d93373f3f0f5f8e5124b90c2e7725ca0cfc897da
MateusGouveia <mateusgouveiadev@gmail.com>
2021-06-03 14:47:29 -0300

    Iniciando Projeto

---
+++ APP_series/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APP_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
